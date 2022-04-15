ca3be9d6d26d93d3f591257ef4a23e2b9c200fae
alisbai <ali.sbai.96@gmail.com>
2022-04-15 22:09:10 +0000

    most of webpack setup is done

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import "./style.css"
+ReactDOM.render(<img src="images/image-hero-mobile.png"/>, document.querySelector("body"));
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "index.bundle.js"
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/template.html"
+            }),
+            new CleanWebpackPlugin()
+],
+    module: {
+        rules: [
+        {
+            test: /\.html$/i,
+            loader: "html-loader"
+        },
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"]
+        },
+        {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env', '@babel/preset-react']
+              }
+            }
+          },
+          {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+              {
+                loader: 'file-loader',
+              },
+            ],
+          },
+        ]
+    }
+
+}
