a9a2eed610e6e286e9be82b4e4db1c2e040de502
alisbai <ali.sbai.96@gmail.com>
2022-04-16 09:25:34 +0000

    Everything is set up for buiding the project

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import "./style.css"
-ReactDOM.render(<img src="images/image-hero-mobile.png"/>, document.querySelector("body"));
+
+ReactDOM.render(<h1>Hello World</h1>, document.querySelector("body"))
